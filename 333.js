c3745810442bdb807cb7555941327a89a2b02a9d
LOLATM <79366095+LOLATM@users.noreply.github.com>
2022-05-31 23:10:22 +0800

    Update 333.js

--- 333.js
+++ 333.js
@@ -13,7 +13,7 @@ const $ = new Env(`前台自动阅读`);
       <body><div id="timer"></div></body>
       <script>
           var oBox= document.getElementById('timer');
-          var maxtime = parseInt(Math.random() * (2 - 2 + 1) + 2);
+          var maxtime = parseInt(Math.random() * (3 - 3 + 1) + 3);
           setTimeout(()=>window.history.back(),maxtime*1000);
           function CountDown() {
               if (maxtime >= 0) {
